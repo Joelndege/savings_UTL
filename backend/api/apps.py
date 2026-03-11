16f1646719b0cea862f75bf562c4670db11943e6
Joel Ndege <ndegejoel2000@gmail.com>
2026-03-12 00:16:47 +0200

    create production superuser

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="admin1234"
+    )
