4e40492e3de6edb995dd6197d2a36234ef72ddff
Joel Ndege <ndegejoel2000@gmail.com>
2026-03-11 23:52:36 +0200

    fix entrypoint for railway

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-echo "[$(date)] --- Starting Entrypoint Script ---"
-
-echo "[$(date)] Running migrations..."
+echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "[$(date)] Collecting static files..."
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "[$(date)] Starting Gunicorn..."
-# Use the PORT environment variable provided by Railway
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --access-logfile - --error-logfile -
-
+echo "Starting server..."
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
