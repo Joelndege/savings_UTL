119d7eae26b85e03e495c491d03edd695095a011
Joel Ndege <ndegejoel2000@gmail.com>
2026-03-12 09:23:56 +0200

    fix entrypoint for railway

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Running migrations..."
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "Starting server..."
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
