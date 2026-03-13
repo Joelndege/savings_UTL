bd649b9c4ac9107b840efae3851eaeff42c71917
Joel Ndege <ndegejoel2000@gmail.com>
2026-03-13 13:29:04 +0200

    fix superuser creation

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,10 +4,13 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Creating superuser..."
+
+export DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD
+
 python manage.py createsuperuser \
---noinput \
---username $DJANGO_SUPERUSER_USERNAME \
---email $DJANGO_SUPERUSER_EMAIL || true
+  --noinput \
+  --username "$DJANGO_SUPERUSER_USERNAME" \
+  --email "$DJANGO_SUPERUSER_EMAIL" || true
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
