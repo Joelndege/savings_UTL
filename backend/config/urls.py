d89d3099017e75c6ab34d39fd9e7099ef7bfa171
Joel Ndege <ndegejoel2000@gmail.com>
2026-03-13 13:37:00 +0200

    fix home

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from api.views_health import health_check
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Savings UTL API is running 🚀")
+
 
 urlpatterns = [
-    path('', health_check, name='root_health'),
-    path('health/', health_check, name='health_check'),
+    path('', home, name='home'),   # homepage
+    path('health/', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-
